07ff9ab1cf47eff05b39a854d78d197270537cc2
Evgeny <e.a.zhuchkov@gmail.com>
2016-07-12 21:30:58 +0300

    Доделал ошибку по обнулению счетчика

--- recursion.js
+++ recursion.js
@@ -2,7 +2,11 @@ let index = 0;
 
 function consoleRec(source) {
     console.log(source[index++]);
-    if (index < source.length) consoleRec(source);
+    if (index < source.length){
+        consoleRec(source)
+    } else {
+        index = 0
+    }
 }
 
 module.exports = consoleRec;
